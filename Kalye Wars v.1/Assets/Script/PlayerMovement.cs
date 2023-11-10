39a817b9b9f6c6f1c9d20713855a0a4673e94e48
Luck3rd <agento.edmon03@gmail.com>
2023-11-10 21:04:09 +0800

    add movement

---
+++ Kalye Wars v.1/Assets/Script/PlayerMovement.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public Animator animator;
+    public Rigidbody2D rb;
+    Vector2 movement;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+
+        movement.Normalize();
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+    }
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+}
